82b25a57bfc53a6a3c74e3056bf7ef0cc86ad0ba
Aureana <111321782+Aureana@users.noreply.github.com>
2022-08-30 15:42:51 -0300

    melhorando as respostas

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -4,15 +4,15 @@ console.log(num1)
 console.log(num2)
 
 resultado = num1 > num2
-console.log("Atenção a opção é:", resultado)
+console.log("O primeiro numero é maior que segundo?:", resultado)
 
 resultado = num1 == num2
-console.log("Atenção a opção é:", resultado)
+console.log("O primeiro numero é igual ao segundo?:", resultado)
 
 resultado = num1 / num2
 resto = ((num1 % num2)==0)
-console.log("Atençã é divisivel por 2:", resto )
+console.log("O primeiro numero é divisível pelo segundo?:", resto )
 
 resultado = num2 / num1
 resto = ((num2 % num1)==0)
-console.log("Atençã é divisivel por 2:", resto )
+console.log("O segundo numero é divisível pelo primeiro?:", resto )
